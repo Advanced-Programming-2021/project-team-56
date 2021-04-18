106d81b98f61b93d2bd22de728e23b4029180b27
Mehrshadbarzamini <mehrbarzamini@gmail.com>
2021-04-18 22:15:32 +0330

    Main with Scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,10 @@
-import java.util.ArrayList;
-import java.util.function.Consumer;
+import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-//        ArrayList<Integer> integers = new ArrayList<>();
-//
-//        integers.forEach(new Consumer<Integer>() {
-//            @Override
-//            public void accept(Integer integer) {
-//
-//            }
-//        });
+
     }
 }
