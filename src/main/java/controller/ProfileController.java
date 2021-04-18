f1975ce34cb5f1a744931044eb13aa089725a742
Mehrshadbarzamini <mehrbarzamini@gmail.com>
2021-04-18 23:54:32 +0330

    Main formatted

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -1,25 +1,20 @@
-﻿package controller ;
+﻿package controller;
 
- class ProfileController
-{
-    
-    private String changePassword(String password) 		
-    {
-        
-    }		
-    
-    private String changeNickname(String nickname) 		
-    {
-        
-    }		
-    
-    public String verifyOrder(String command) 		
-    {
-        
-    }		
-    
-    private String showCurrentMenu() 		
-    {
-        
-    }		
+class ProfileController {
+
+    private String changePassword(String password) {
+
+    }
+
+    private String changeNickname(String nickname) {
+
+    }
+
+    public String verifyOrder(String command) {
+
+    }
+
+    private String showCurrentMenu() {
+
+    }
 }
--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -4,36 +4,30 @@ import java.util.ArrayList;
 
 ;
 
- class Deck
-{
+class Deck {
     private ArrayList<Card> mainDeck;
-    private ArrayList<Card> sideDeck;		
-    private boolean isActivated;		
-    private String deckName;		
+    private ArrayList<Card> sideDeck;
+    private boolean isActivated;
+    private String deckName;
 
-    
-    public Deck(String deckName)
-    {
-        
-    }		
-    
-    public String getDeckName() 		
-    {
-        
-    }		
-    
-    public void setIsActive() 		
-    {
-        
-    }		
-    
-    public void addCardToSideDeck(String cardName) 		
-    {
-        
-    }		
-    
-    public void addCardToMainDeck(String cardName) 		
-    {
-        
-    }		
+
+    public Deck(String deckName) {
+
+    }
+
+    public String getDeckName() {
+
+    }
+
+    public void setIsActive() {
+
+    }
+
+    public void addCardToSideDeck(String cardName) {
+
+    }
+
+    public void addCardToMainDeck(String cardName) {
+
+    }
 }
