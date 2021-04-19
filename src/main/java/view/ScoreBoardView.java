ec7e4277c74cf109e5aea517076d05e7291a57e8
amirrzw <amirrezaabotalebi81@gmail.com>
2021-04-19 22:52:53 +0430

    menus

--- src/main/java/view/ScoreBoardView.java
+++ src/main/java/view/ScoreBoardView.java
@@ -1,6 +1,9 @@
 ﻿package view;
 
+import controller.ScoreBoardController;
 import model.User;
+
+import java.util.regex.Matcher;
 
 public class ScoreBoardView {
 
@@ -16,6 +19,34 @@ public class ScoreBoardView {
     }
 
     public void run(User user) {
+        String command;
+        while (true) {
+            command = LoginMenuView.scan.nextLine();
+            command = command.trim();
 
+            if (command.equals("menu show-current")) {
+                System.out.println("Scoreboard");
+                continue;
+            }
+            if (command.equals("menu exit")) {
+                break;
+            }
+            String regex = "^menu enter (?:Duel|Deck|Scoreboard|Profile|Shop|Import\\/Export)$";
+            Matcher matcher = LoginMenuView.getMatcher(command, regex);
+            if (matcher.find()) {
+                System.out.println("menu navigation is not possible");
+                continue;
+            }
+            if (command.equals("scoreboard show")) {
+                showScoreBoard();
+                continue;
+            }
+            System.out.println("invalid command");
+        }
+    }
+
+    private void showScoreBoard() {
+        String scoreBoard = ScoreBoardController.getInstance().showScoreBoard();
+        System.out.print(scoreBoard);
     }
 }
