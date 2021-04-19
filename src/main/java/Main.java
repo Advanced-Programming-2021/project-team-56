2348abe59cdb9f21ce16a050bc8014c33d0273f0
Mehrshadbarzamini <mehrbarzamini@gmail.com>
2021-04-19 08:14:31 +0330

    ProfileView changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner mainScanner = new Scanner(System.in);
     public void main(String[] args) {
         LoginMenuView loginMenuView = new LoginMenuView();
         loginMenuView.run();
--- src/main/java/view/ProfileView.java
+++ src/main/java/view/ProfileView.java
@@ -4,7 +4,21 @@ import model.User;
 
 public class ProfileView {
 
-    public void run(User user) {
+    public void run(String username) {
+        String command;
+        while (true) {
+            command = LoginMenuView.scan.nextLine();
+            command = command.trim();
+            if (command.equals("menu exit")) {
+                return;
+            }
+            if (command.equals("menu show-current")) {
+                System.out.println("Profile Menu");
+                continue;
+            }
+            if (command.startsWith("")) {
 
+            }
+        }
     }
 }
