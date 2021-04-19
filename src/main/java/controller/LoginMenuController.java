18720e6434cadeb1ecf29d949a720131bdeaffe5
amirrzw <amirrezaabotalebi81@gmail.com>
2021-04-19 18:50:30 +0430

    login menu

--- src/main/java/controller/LoginMenuController.java
+++ src/main/java/controller/LoginMenuController.java
@@ -1,5 +1,7 @@
 ﻿package controller;
 
+import model.User;
+
 public class LoginMenuController {
 
     private static LoginMenuController loginMenuController;
@@ -14,8 +16,15 @@ public class LoginMenuController {
     }
 
 
-    public String logIn() {
-
+    public String logIn(String username, String password) {
+        User user = User.getUserByUsername(username);
+        if (user == null) {
+            return "Username and password didn’t match!";
+        }
+        if (user.getPassword().equals(password)) {
+            return "Username and password didn’t match!";
+        }
+        return "user logged in successfully!";
     }
 
     public String showCurrentMenu() {
@@ -23,6 +32,13 @@ public class LoginMenuController {
     }
 
     public String register(String username, String password, String nickname) {
-
+        if (User.isThisUsernameAlreadyTaken(username)) {
+            return "user with username " + username + " already exists";
+        }
+        if (User.isThisNicknameAlreadyTaken(nickname)) {
+            return "user with nickname " + nickname + " already exists";
+        }
+        User user = new User(username, nickname, password);
+        return "user created successfully!";
     }
 }
