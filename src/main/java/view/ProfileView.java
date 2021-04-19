dd548f0b2a22bc82e619adf5a65ca766b996dd0d
Mehrshadbarzamini <mehrbarzamini@gmail.com>
2021-04-19 10:34:21 +0330

    new Codes in profile view and controller

--- src/main/java/view/ProfileView.java
+++ src/main/java/view/ProfileView.java
@@ -1,10 +1,14 @@
 ﻿package view;
 
+import controller.ProfileController;
 import model.User;
+
+import java.util.regex.Matcher;
 
 public class ProfileView {
 
     public void run(String username) {
+        User user = User.getUserByUsername(username);
         String command;
         while (true) {
             command = LoginMenuView.scan.nextLine();
@@ -17,8 +21,27 @@ public class ProfileView {
                 continue;
             }
             if (command.startsWith("profile change --nickname")) {
-                
+                checkProfileChangeNicknameCommand(username, command);
+                continue;
+            }
+            if (command.startsWith("profile change")) {
+                checkProfileChangePasswordCommand(username, command);
             }
         }
+    }
+
+    private void checkProfileChangeNicknameCommand(String username, String command) {
+        Matcher matcher = LoginMenuView.getMatcher(command, "^profile change --nickname (\\S+)$");
+        if (matcher.find()) {
+            String newNickname = matcher.group(1);
+            String result = ProfileController.changeNickname(username, newNickname);
+            System.out.println(result);
+        } else {
+            System.out.println("invalid command");
+        }
+    }
+
+    private void checkProfileChangePasswordCommand(String username, String command) {
+        Matcher matcher = LoginMenuView.getMatcher(command, "");
     }
 }
