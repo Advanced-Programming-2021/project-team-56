1be8ecd71af540849f06b1b0dfc7581491e4fa1a
amiralifarahani <amirali40farahani40@gmail.com>
2021-04-20 18:17:07 +0430

    ProfileController and ProfileView changed

--- src/main/java/view/ProfileView.java
+++ src/main/java/view/ProfileView.java
@@ -11,12 +11,6 @@ public class ProfileView {
     private static ProfileView profileView;
     static Pattern menuEnter = Pattern.compile("^menu enter (?:Login|Duel|Deck|Scoreboard|Profile|Shop|Import/Export)$");
     static Pattern changeNickname = Pattern.compile("^profile change --nickname (\\S+)$");
-    static Pattern changePassword1 = Pattern.compile("^profile change --password --current (\\S+) --new (\\S+)$");
-    static Pattern changePassword2 = Pattern.compile("^profile change --current (\\S+) --password --new (\\S+)$");
-    static Pattern changePassword3 = Pattern.compile("^profile change --current (\\S+) --new (\\S+) --password$");
-    static Pattern changePassword4 = Pattern.compile("^profile change --password --new (\\S+) --current (\\S+)$");
-    static Pattern changePassword5 = Pattern.compile("^profile change --new (\\S+) --password --current (\\S+)$");
-    static Pattern changePassword6 = Pattern.compile("^profile change --new (\\S+) --current (\\S+) --password$");
 
     private ProfileView() {
     }
@@ -28,11 +22,13 @@ public class ProfileView {
     }
 
     public void run(String username) {
+        ProfileController profileController = ProfileController.getInstance();
         String command;
         while (true) {
             command = LoginMenuView.scan.nextLine().trim();
-            if (command.startsWith("menu enter")) {
-                checkMenuEnterCommand(command);
+            Matcher matcher = menuEnter.matcher(command);
+            if (matcher.find()) {
+                System.out.println("menu navigation is not possible");
                 continue;
             }
             if (command.equals("menu exit")) {
@@ -47,17 +43,8 @@ public class ProfileView {
                 continue;
             }
             if (command.startsWith("profile change")) {
-                checkProfileChangePasswordCommand(username, command);
+                System.out.println(profileController.checkProfileChangePasswordCommand(username, command));
             }
-        }
-    }
-
-    private void checkMenuEnterCommand(String command) {
-        Matcher matcher = menuEnter.matcher(command);
-        if (matcher.find()) {
-            System.out.println("menu navigation is not possible");
-        } else {
-            System.out.println("invalid command");
         }
     }
 
@@ -65,57 +52,11 @@ public class ProfileView {
         Matcher matcher = changeNickname.matcher(command);
         if (matcher.find()) {
             String newNickname = matcher.group(1);
-            String result = ProfileController.getInstance().changeNickname(username, newNickname);
-            System.out.println(result);
+            System.out.println(ProfileController.getInstance().changeNickname(username, newNickname));
         } else {
             System.out.println("invalid command");
         }
     }
 
-    private void checkProfileChangePasswordCommand(String username, String command) {
-        Matcher matcher = changePassword1.matcher(command);
-        if (matcher.find()) {
-            checkPasswords(matcher.group(1), matcher.group(2), username);
-            return;
-        }
-        matcher = changePassword2.matcher(command);
-        if (matcher.find()) {
-            checkPasswords(matcher.group(1), matcher.group(2), username);
-            return;
-        }
-        matcher = changePassword3.matcher(command);
-        if (matcher.find()) {
-            checkPasswords(matcher.group(1), matcher.group(2), username);
-            return;
-        }
-        matcher = changePassword4.matcher(command);
-        if (matcher.find()) {
-            checkPasswords(matcher.group(2), matcher.group(1), username);
-            return;
-        }
-        matcher = changePassword5.matcher(command);
-        if (matcher.find()) {
-            checkPasswords(matcher.group(2), matcher.group(1), username);
-            return;
-        }
-        matcher = changePassword6.matcher(command);
-        if (matcher.find()) {
-            checkPasswords(matcher.group(2), matcher.group(1), username);
-            return;
-        }
-        System.out.println("invalid command");
-    }
 
-    private void checkPasswords(String currentPassword, String newPassword, String username) {
-        if (User.getUserByUsername(username).getPassword().equals(currentPassword)) {
-            if (currentPassword.equals(newPassword)) {
-                System.out.println("please enter a new password");
-            } else {
-                ProfileController.getInstance().changePassword(newPassword, username);
-                System.out.println("password changed successfully");
-            }
-        } else {
-            System.out.println("current password is invalid");
-        }
-    }
 }
