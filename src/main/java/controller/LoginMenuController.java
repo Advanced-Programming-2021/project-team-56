11efb1ed217698de9c545609f5ac7eb1c0332582
Mehrshadbarzamini <mehrbarzamini@gmail.com>
2021-04-20 12:04:39 +0330

    AmirReza Mehrshad menus

--- src/main/java/controller/LoginMenuController.java
+++ src/main/java/controller/LoginMenuController.java
@@ -1,5 +1,7 @@
 ﻿package controller;
 
+import model.User;
+
 public class LoginMenuController {
 
     private static LoginMenuController loginMenuController;
@@ -14,11 +16,25 @@ public class LoginMenuController {
     }
 
 
-    public String logIn() {
-
+    public String logIn(String username, String password) {
+        User user = User.getUserByUsername(username);
+        if (user == null) {
+            return "Username and password didn’t match!";
+        }
+        if (user.getPassword().equals(password)) {
+            return "Username and password didn’t match!";
+        }
+        return "user logged in successfully!";
     }
 
     public String register(String username, String password, String nickname) {
-
+        if (User.isThisUsernameAlreadyTaken(username)) {
+            return "user with username " + username + " already exists";
+        }
+        if (User.isThisNicknameAlreadyTaken(nickname)) {
+            return "user with nickname " + nickname + " already exists";
+        }
+        new User(username, nickname, password);
+        return "user created successfully!";
     }
 }
