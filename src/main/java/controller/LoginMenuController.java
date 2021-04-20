182e8335f79e7eb4530afdddbdf94bf567a94e21
amiralifarahani <amirali40farahani40@gmail.com>
2021-04-20 15:59:03 +0430

    changes functions class

--- src/main/java/controller/LoginMenuController.java
+++ src/main/java/controller/LoginMenuController.java
@@ -2,9 +2,18 @@
 
 import model.User;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginMenuController {
 
     private static LoginMenuController loginMenuController;
+    static Pattern createUser1 = Pattern.compile("^user create --username (\\S+) --password (\\S+) --nickname (\\S+)$");
+    static Pattern createUser2 = Pattern.compile("^user create --username (\\S+) --nickname (\\S+) --password (\\S+)$");
+    static Pattern createUser3 = Pattern.compile("^user create --password (\\S+) --nickname (\\S+) --username (\\S+)$");
+    static Pattern createUser4 = Pattern.compile("^user create --password (\\S+) --username (\\S+) --nickname (\\S+)$");
+    static Pattern createUser5 = Pattern.compile("^user create --nickname (\\S+) --username (\\S+) --password (\\S+)$");
+    static Pattern createUser6 = Pattern.compile("^user create --nickname (\\S+) --password (\\S+) --username (\\S+)$");
 
     private LoginMenuController() {
     }
@@ -36,5 +45,35 @@ public class LoginMenuController {
         }
         new User(username, nickname, password);
         return "user created successfully!";
+    }
+
+    public String createUser(String command) {
+        String result = "";
+        Matcher matcher = createUser1.matcher(command);
+        if (matcher.find()) {
+            result = register(matcher.group(1), matcher.group(2), matcher.group(3));
+        }
+        matcher = createUser2.matcher(command);
+        if (matcher.find()) {
+            result = register(matcher.group(1), matcher.group(3), matcher.group(2));
+        }
+        matcher = createUser3.matcher(command);
+        if (matcher.find()) {
+            result = register(matcher.group(3), matcher.group(1), matcher.group(2));
+        }
+        matcher = createUser4.matcher(command);
+        if (matcher.find()) {
+            result = register(matcher.group(2), matcher.group(1), matcher.group(3));
+        }
+        matcher = createUser5.matcher(command);
+        if (matcher.find()) {
+            result = register(matcher.group(2), matcher.group(3), matcher.group(1));
+        }
+        matcher = createUser6.matcher(command);
+        if (matcher.find()) {
+            result = register(matcher.group(3), matcher.group(2), matcher.group(1));
+        }
+        if (result.equals("")) return "invalid command";
+        else return result;
     }
 }
--- src/main/java/view/LoginMenuView.java
+++ src/main/java/view/LoginMenuView.java
@@ -10,12 +10,6 @@ public class LoginMenuView {
 
     private static LoginMenuView loginMenuView;
     static Pattern menuEnter = Pattern.compile("^menu enter (?:Login|Duel|Deck|Scoreboard|Profile|Shop|Import\\/Export)$");
-    static Pattern createUser1 = Pattern.compile("^user create --username (\\S+) --password (\\S+) --nickname (\\S+)$");
-    static Pattern createUser2 = Pattern.compile("^user create --username (\\S+) --nickname (\\S+) --password (\\S+)$");
-    static Pattern createUser3 = Pattern.compile("^user create --password (\\S+) --nickname (\\S+) --username (\\S+)$");
-    static Pattern createUser4 = Pattern.compile("^user create --password (\\S+) --username (\\S+) --nickname (\\S+)$");
-    static Pattern createUser5 = Pattern.compile("^user create --nickname (\\S+) --username (\\S+) --password (\\S+)$");
-    static Pattern createUser6 = Pattern.compile("^user create --nickname (\\S+) --password (\\S+) --username (\\S+)$");
     static Pattern loginUser = Pattern.compile("^user login (\\S+) (\\S+) (\\S+) (\\S+)$");
 
     private LoginMenuView() {
@@ -30,6 +24,7 @@ public class LoginMenuView {
     public static Scanner scan = new Scanner(System.in);
 
     public void run() {
+        LoginMenuController loginMenuController = LoginMenuController.getInstance();
         String command;
         while (true) {
             command = scan.nextLine().trim();
@@ -38,7 +33,7 @@ public class LoginMenuView {
                 continue;
             }
             if (command.startsWith("user create")) {
-                createUser(command);
+                System.out.println(loginMenuController.createUser(command));
                 continue;
             }
             if (command.startsWith("user login")) {
@@ -55,37 +50,6 @@ public class LoginMenuView {
             }
             System.out.println("invalid command");
         }
-    }
-
-    private void createUser(String command) {
-        LoginMenuController loginMenuController = LoginMenuController.getInstance();
-        String result = "";
-        Matcher matcher = createUser1.matcher(command);
-        if (matcher.find()) {
-            result = loginMenuController.register(matcher.group(1), matcher.group(2), matcher.group(3));
-        }
-        matcher = createUser2.matcher(command);
-        if (matcher.find()) {
-            result = loginMenuController.register(matcher.group(1), matcher.group(3), matcher.group(2));
-        }
-        matcher = createUser3.matcher(command);
-        if (matcher.find()) {
-            result = loginMenuController.register(matcher.group(3), matcher.group(1), matcher.group(2));
-        }
-        matcher = createUser4.matcher(command);
-        if (matcher.find()) {
-            result = loginMenuController.register(matcher.group(2), matcher.group(1), matcher.group(3));
-        }
-        matcher = createUser5.matcher(command);
-        if (matcher.find()) {
-            result = loginMenuController.register(matcher.group(2), matcher.group(3), matcher.group(1));
-        }
-        matcher = createUser6.matcher(command);
-        if (matcher.find()) {
-            result = loginMenuController.register(matcher.group(3), matcher.group(2), matcher.group(1));
-        }
-        if (result.equals("")) System.out.println("invalid command");
-        else System.out.println(result);
     }
 
     private void loginUser(String command) {
