6171457f61d93f96ff215a77d4c77a687b612bbc
amiralifarahani <amirali40farahani40@gmail.com>
2021-04-21 17:49:10 +0430

    some changes in excel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import data.ExcelUtils;
+import model.MonsterCard;
 import view.LoginMenuView;
 
 import java.io.IOException;
 
 public class Main {
     public void main(String[] args) throws IOException {
-        ExcelUtils.getInstance().getRowCount();
+        ExcelUtils.getInstance().run();
+        System.out.println(MonsterCard.getMonsterCards().size());
         LoginMenuView.getInstance().run();
     }
 }
--- src/main/java/data/ExcelUtils.java
+++ src/main/java/data/ExcelUtils.java
@@ -1,32 +1,91 @@
 package data;
 
+import model.MonsterCard;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ExcelUtils {
     private static ExcelUtils excelUtils;
+
     private ExcelUtils() {
     }
 
-    public static ExcelUtils getInstance(){
-        if (excelUtils == null){
+    public static ExcelUtils getInstance() {
+        if (excelUtils == null) {
             excelUtils = new ExcelUtils();
         }
         return excelUtils;
     }
 
-    public void getRowCount(){
-        String excelPath = "./data/Monster.csv";
-        XSSFWorkbook workBook = null;
-        try {
-            workBook = new XSSFWorkbook(excelPath);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static String name;
+    private static String attribute;
+    private static int level;
+    private static int price;
+    private static String description;
+    private static int attack;
+    private static int defence;
+    private static String cardType;
+    private static String monsterType;
+
+    public void run() throws IOException {
+        String prjDir = System.getProperty("user.dir");
+        String excelPath = prjDir + "/data/Monster.xlsx";
+        FileInputStream files = new FileInputStream(new File(excelPath));
+        XSSFWorkbook workbook = new XSSFWorkbook(files);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        createMonsterCardObjects(sheet);
+    }
+
+    private void createMonsterCardObjects(XSSFSheet sheet){
+        ArrayList<MonsterCard> monsterCards = MonsterCard.getMonsterCards();
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+            MonsterCard monsterCard = new MonsterCard();
+            for (int j = 0; j < sheet.getRow(i).getPhysicalNumberOfCells(); j++) {
+                switch (j) {
+                    case 0:
+                        name = sheet.getRow(i).getCell(j).toString();
+                        break;
+                    case 1:
+                        level = (int) sheet.getRow(i).getCell(j).getNumericCellValue();
+                        monsterCard.setLevel(level);
+                        break;
+                    case 2:
+                        attribute = sheet.getRow(i).getCell(j).toString();
+                        monsterCard.setAttribute(attribute);
+                        break;
+                    case 3:
+                        monsterType = sheet.getRow(i).getCell(j).toString();
+                        monsterCard.setMonsterType(monsterType);
+                        break;
+                    case 4:
+                        cardType = sheet.getRow(i).getCell(j).toString();
+                        monsterCard.setCardType(cardType);
+                        break;
+                    case 5:
+                        attack = (int) sheet.getRow(i).getCell(j).getNumericCellValue();
+                        System.out.println(attack);
+                        monsterCard.setAttack(attack);
+                        break;
+                    case 6:
+                        defence = (int) sheet.getRow(i).getCell(j).getNumericCellValue();
+                        monsterCard.setDefence(defence);
+                        break;
+                    case 7:
+                        description = sheet.getRow(i).getCell(j).toString();
+                        System.out.println(description);
+                        break;
+                    case 8:
+                        price = (int) sheet.getRow(i).getCell(j).getNumericCellValue();
+                        monsterCard.setPrice(price);
+                        break;
+                }
+            }
+            monsterCards.add(monsterCard);
         }
-        XSSFSheet sheet = workBook.getSheet("Monster");
-        int rowCount = sheet.getPhysicalNumberOfRows();
-        System.out.println("No of rows : " + rowCount);
     }
 }
