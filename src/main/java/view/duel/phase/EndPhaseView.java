beae5c361722644d2211987d81c9f9bfd0ab2e05
amiralifarahani <amirali40farahani40@gmail.com>
2021-05-01 19:17:20 +0430

    end phase is done

--- src/main/java/view/duel/phase/EndPhaseView.java
+++ src/main/java/view/duel/phase/EndPhaseView.java
@@ -1,5 +1,9 @@
 package view.duel.phase;
 
+import controller.duel.DuelWithUser;
+import controller.duel.phases.DrawPhaseController;
+import view.LoginMenuView;
+
 public class EndPhaseView {
     private static EndPhaseView endPhase;
 
@@ -14,7 +18,16 @@ public class EndPhaseView {
         return endPhase;
     }
 
-    public void run(){
-
+    public void run() {
+        System.out.println("phase: End Phase");
+        while (true) {
+            String command = LoginMenuView.scan.nextLine().trim();
+            if (command.equals("next phase")) {
+                break;
+            }
+            System.out.println("invalid command");
+        }
+        String nickname = DuelWithUser.getInstance().getEnemyBoard().getUser().getNickname();
+        System.out.println("its " + nickname + "’s turn");
     }
 }
