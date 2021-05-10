ca585a1231270c57a2fcf8ed49a3927706458fbc
amirrzw <amirrezaabotalebi81@gmail.com>
2021-05-10 14:27:54 +0430

    spell efect update

--- src/main/java/controller/duel/effects/SpellEffectCanActivate.java
+++ src/main/java/controller/duel/effects/SpellEffectCanActivate.java
@@ -1,4 +1,16 @@
 package controller.duel.effects;
 
 public class SpellEffectCanActivate {
+
+    private static SpellEffectCanActivate spellEffectCanActivate;
+
+    private SpellEffectCanActivate() {
+
+    }
+
+    public static SpellEffectCanActivate getInstance() {
+        if (spellEffectCanActivate == null)
+            spellEffectCanActivate = new SpellEffectCanActivate();
+        return spellEffectCanActivate;
+    }
 }
