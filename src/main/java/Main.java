a0b372b2987bf725eb4dcde257c186b818669c3a
amirrzw <amirrezaabotalebi81@gmail.com>
2021-05-15 22:01:18 +0430

    json update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import controller.LoginMenuController;
 import view.LoginMenuView;
 
 import java.io.IOException;
@@ -6,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ExcelUtils.getInstance().run();
+        LoginMenuController.getInstance().readFromJson();
         LoginMenuView.getInstance().run();
+        LoginMenuController.getInstance().updateJson();
     }
 }
