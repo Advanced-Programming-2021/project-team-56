127c98af304c5f3d2f12c5a6eb34ecd26f623e48
amirrzw <amirrezaabotalebi81@gmail.com>
2021-05-15 17:48:57 +0430

    update all

--- src/main/java/controller/LoginMenuController.java
+++ src/main/java/controller/LoginMenuController.java
@@ -1,7 +1,15 @@
 package controller;
 
+import com.gilecode.yagson.YaGson;
+import com.gilecode.yagson.com.google.gson.reflect.TypeToken;
 import model.User;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -75,5 +83,29 @@ public class LoginMenuController {
         }
         if (result.equals("")) return "invalid command";
         else return result;
+    }
+
+    public void readFromJson() {
+        try {
+            String json = new String(Files.readAllBytes(Paths.get("json.json")));
+            ArrayList<User> users = new YaGson().fromJson(json,
+                    new TypeToken<List<User>>(){}.getType()
+            );
+            for (User user : users) {
+                User.getUsers().add(user);
+            }
+        } catch (IOException e) {
+            System.out.println("can't read from json");
+        }
+    }
+
+    public void updateJson() {
+        try {
+            FileWriter writer = new FileWriter("json.json");
+            writer.write(new YaGson().toJson(User.getUsers()));
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("can't create or update json");
+        }
     }
 }
