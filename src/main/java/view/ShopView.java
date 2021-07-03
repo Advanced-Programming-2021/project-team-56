94e7d590b4a82b0e4c238c4485a05fcef202afdc
amirrzw <amirrezaabotalebi81@gmail.com>
2021-07-03 18:03:36 +0430

    update shop menu

--- src/main/java/view/ShopView.java
+++ src/main/java/view/ShopView.java
@@ -1,8 +1,10 @@
 package view;
 
+import controller.ShopController;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -17,13 +19,18 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import model.Card;
 import model.User;
+import model.enums.MenuURL;
+import view.components.NodeEditor;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class ShopView {
 
-
+    public static Card currentCard;
     public ScrollPane cardsScrollPane;
     public GridPane cardsGridPane;
     public HBox cardsHBox;
@@ -32,16 +39,21 @@ public class ShopView {
     public ImageView cardImage;
     public Label numberOfCardLabel;
     public Button buyButton;
+    public Label errorLabel;
+    public Button backButton;
+    public Label capitalLabel;
 
     @FXML
     public void initialize() {
-        //530b80
         cardsGridPane.setStyle("-fx-background-color: #41415D");
         setHBoxBackGround();
-        addMonsterCards();
+        addCards();
+        setOnMouseEnteredAndExited(buyButton);
+        setOnMouseEnteredAndExited(backButton);
+        capitalLabel.setText(String.valueOf(User.getCurrentUser().getMoney()));
     }
 
-    private void addMonsterCards() {
+    private void addCards() {
         int row = 0;
         int column = 0;
         for (int i = 0; i < 76; i++) {
@@ -49,7 +61,7 @@ public class ShopView {
                 column = 0;
                 row++;
             }
-            Rectangle rectangle = new Rectangle(160,210);
+            Rectangle rectangle = new Rectangle(160, 210);
             setOnMouseEnteredAndExited(rectangle);
             setOnMouseClicked(rectangle, i);
             Image image = new Image(Card.getCards().get(i).getImageURL());
@@ -67,28 +79,28 @@ public class ShopView {
                 false,
                 true);
         Image image = new Image(getClass().getResource("/images/Black-Purple-Hexagon-Background.png").toExternalForm());
-        BackgroundImage backgroundImage = new BackgroundImage(image,  BackgroundRepeat.NO_REPEAT,
+        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
                 backgroundSize);
         cardsHBox.setBackground(new Background(backgroundImage));
     }
 
-    private void setOnMouseEnteredAndExited(Rectangle rectangle) {
-        rectangle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+    private void setOnMouseEnteredAndExited(Node node) {
+        node.setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                DropShadow borderGlow= new DropShadow();
+                DropShadow borderGlow = new DropShadow();
                 borderGlow.setColor(Color.RED);
                 borderGlow.setWidth(70);
                 borderGlow.setHeight(70);
-                rectangle.setEffect(borderGlow);
+                node.setEffect(borderGlow);
             }
         });
-        rectangle.setOnMouseExited(new EventHandler<MouseEvent>() {
+        node.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                rectangle.setEffect(null);
+                node.setEffect(null);
             }
         });
     }
@@ -97,6 +109,7 @@ public class ShopView {
         rectangle.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                currentCard = Card.getCards().get(cardIndex);
                 cardNameLabel.setText(Card.getCards().get(cardIndex).getName());
                 cardPriceLabel.setText(String.valueOf(Card.getCards().get(cardIndex).getPrice()));
                 Image image = new Image(Card.getCards().get(cardIndex).getImageURL());
@@ -108,5 +121,20 @@ public class ShopView {
                 numberOfCardLabel.setText(String.valueOf(numberOfCard));
             }
         });
+    }
+
+    public void buyClicked(MouseEvent mouseEvent) {
+        String result = ShopController.getInstance().buyCard(currentCard.getName());
+        if (result.equals("")) {
+            errorLabel.setText("buying was successfully");
+            capitalLabel.setText(String.valueOf(User.getCurrentUser().getMoney()));
+            int numberOfCard = Integer.parseInt(numberOfCardLabel.getText()) + 1;
+            numberOfCardLabel.setText(String.valueOf(numberOfCard));
+        }
+        else errorLabel.setText(result);
+    }
+
+    public void backClicked(MouseEvent mouseEvent) throws IOException {
+        FxmlController.getInstance().setSceneFxml(MenuURL.MAIN);
     }
 }
